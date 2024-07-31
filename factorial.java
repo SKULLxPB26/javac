b5fe330edbd797043df9bf9f542aba42ac0cda01
TaranMehra <taranimpworks@gmail.com>
2024-07-31 09:03:30 +0530

    factorail java program

---
+++ factorial.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class factorial {
+    public static void main(String[] args) {
+        
+        Scanner sca = new Scanner(System.in);
+        try {
+            
+            System.out.println("enterend the number");
+            int f = sca.nextInt();
+                
+
+        }
+        catch (Exception e) {
+            System.out.println(e);
+        }
+
+    }
+
+    
+}
